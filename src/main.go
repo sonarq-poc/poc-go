1c8dffec85202fb66febdb04cd7003fb25d2bad4
Adron Nguyen <adron.nguyen@ag-pc0028-m.na.corp.samsungelectronics.net>
2023-02-07 16:03:14 -0500

    [DEGA-XXXX] Attempt to add more code smells

--- src/main.go
+++ src/main.go
@@ -2,6 +2,46 @@ package main
 
 import "fmt"
 
+var x = 10
+
 func main() {
     fmt.Println("Go Proof of concept")
+	fmt.Println("Value of x:", x)
+		foo() 
+	fmt.Println("Value of x:", x)
+	foo2()
+	foo2()
+
+	fmt.Println("Value of x:", x)
+	fmt.Println("Value of x:", x)
 }
+
+	
+
+			
+
+func addition1() {
+	var a int = 1
+	var b int = 10
+	var c int
+	c = a +b
+
+	fmt.Printf("Value of c is %d\n", c)
+}
+
+func addition2() {
+	var a int = 1
+	var b int = 10
+	var c int
+	c = a +b
+	fmt.Printf("Value of c is %d\n", c)
+}
+
+func foo2() {
+		x =x-5
+}
+
+func foo() {
+	x = x + 2
+}
+
