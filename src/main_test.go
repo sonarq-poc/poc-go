a8dbddc3dcd8e2d65408e6dafe546625740e4756
Mariana Souza <m.souza@samsung.com>
2023-02-14 13:24:41 -0500

    test

--- src/main_test.go
+++ src/main_test.go
@@ -12,3 +12,25 @@ func TestAddition3(t *testing.T) {
         t.Error(sum)
     }
 }
+
+// func TestAddInt(t *testing.T) { 
+//     testCases := []struct { 
+//         Name     string 
+//         Values   []int 
+//         Expected int 
+//     }{ 
+//         {"addInt() -> 0", []int{}, 0}, 
+//         {"addInt([]int{10, 20, 100}) -> 130", []int{10, 20, 100}, 130}, 
+//     } 
+ 
+//     for _, tc := range testCases { 
+//         t.Run(tc.Name, func(t *testing.T) { 
+//             sum := addInt(tc.Values...) 
+//             if sum != tc.Expected { 
+//                 t.Errorf("%d != %d", sum, tc.Expected) 
+//             } else { 
+//                 t.Logf("%d == %d", sum, tc.Expected) 
+//             } 
+//         }) 
+//     } 
+// }
